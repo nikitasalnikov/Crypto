73652932f47bf89359df788df8530f598c2c4bc1
Nikita <nikitasalnikov@yandex.ru>
2022-03-31 12:56:38 +0500

    crypto

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,90 @@
-/***accordion***/
+let burger = document.querySelector(".burger__menu"),
+  navList = document.querySelector(".header__nav-list"),
+  headerLink = document.querySelectorAll(".header__nav-link"),
+  body = document.querySelector("body");
 
+let mySwiper;
 var acc = document.getElementsByClassName("accordion");
 var i;
+
+("use strict");
+
+window.onload = function () {
+  const headerContent = document.querySelector(".header__content");
+
+  if (headerContent) {
+    const blueCandles = document.querySelector(".header__content-bg");
+
+    //Коэффиценты
+    const forCandles = 40;
+
+    //Скорость анимации
+    const speed = 1;
+
+    //обьявление переменных
+
+    let positionX = 0,
+      positionY = 0;
+    let coordXprocent = 0,
+      coordYprocent = 0;
+
+    function setMouseParallaxStyle() {
+      const distX = coordXprocent - positionX;
+      const distY = coordYprocent - positionY;
+
+      positionX = positionX + distX * speed;
+      positionY = positionY + distY * speed;
+
+      blueCandles.style.cssText = `
+        transform: translate(${positionX / forCandles}%, ${
+        positionY / forCandles
+      }%);
+      transition: 0.5s;
+      `;
+
+      requestAnimationFrame(setMouseParallaxStyle);
+    }
+    setMouseParallaxStyle();
+
+    headerContent.addEventListener("mouseover", function (e) {
+      const parallaxWidth = headerContent.offsetWidth;
+      const parallaxHeight = headerContent.offsetHeight;
+
+      const coordX = e.pageX - parallaxWidth / 2;
+      const coordY = e.pageY - parallaxHeight / 2;
+
+      coordXprocent = (coordX / parallaxWidth) * 100;
+      coordYprocent = (coordY / parallaxHeight) * 100;
+    });
+
+    let thresholdSets = [];
+
+    for (let i = 0; i <= 1; i += 0.05) {
+      thresholdSets.push(i);
+    }
+    const callback = function (entries, observer) {
+      const scrollTopProcent =
+        (window.pageYOffset / headerContent.offsetHeight) * 100;
+      setParallaxItemsStyle(scrollTopProcent);
+    };
+    const observer = new IntersectionObserver(callback, {
+      threshold: thresholdSets,
+    });
+    observer.observe(blueCandles);
+
+    function setParallaxItemsStyle(scrollTopProcent) {
+      // content.style.cssText = `transform: translate(0%, -${
+      //   scrollTopProcent / 6
+      // }%);`;
+      blueCandles.style.cssText = `transform: translate(0%, -${
+        scrollTopProcent / 6
+      }%);
+      transition: 0.5s;`;
+    }
+  }
+};
+
+/***accordion***/
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
@@ -16,11 +99,6 @@ for (i = 0; i < acc.length; i++) {
 }
 
 /***burger menu***/
-
-let burger = document.querySelector(".burger__menu"),
-  navList = document.querySelector(".header__nav-list"),
-  headerLink = document.querySelectorAll(".header__nav-link"),
-  body = document.querySelector("body");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
@@ -47,8 +125,6 @@ for (let i = 0; i < headerLink.length; i++) {
 /****slider****/
 
 const slider = document.querySelector(".swiper");
-
-let mySwiper;
 
 function mobileSlider() {
   if (window.innerWidth <= 768 && slider.dataset.mobile == "false") {
