69775ff71fa2ae6d48e10e5a2134447c3827c4c3
Nikita <nikitasalnikov@yandex.ru>
2022-04-05 13:52:15 +0500

    crypto

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,52 @@ let burger = document.querySelector(".burger__menu"),
   navList = document.querySelector(".header__nav-list"),
   headerLink = document.querySelectorAll(".header__nav-link"),
   body = document.querySelector("body");
+animItems = document.querySelectorAll(".anim_items");
 
 let mySwiper;
 var acc = document.getElementsByClassName("accordion");
 var i;
 
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animScroll);
+  function animScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        pageYOffset > animItemOffset - animItemPoint &&
+        pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("active");
+      } else {
+        if (!animItem.classList.contains("anim__no-hide")) {
+          animItem.classList.remove("active");
+        }
+      }
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYoffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+
+  setTimeout(() => {
+    animScroll();
+  }, 300);
+}
+
+/**parallax**/
 ("use strict");
 
 window.onload = function () {
